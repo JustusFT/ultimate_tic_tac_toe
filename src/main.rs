4c1f0f059e6d469b48140cc5cc50f486fd4d120f
Justus Tumacder <JustusFT@gmail.com>
2019-07-13 22:41:21 +0800

    Add board display template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::io::{stdout, Write};
+use termion::clear;
+use termion::cursor;
+use termion::raw::IntoRawMode;
+
+const BOARD_DISPLAY: &'static str = "   │   │    ┃    │   │    ┃    │   │   \r
+───┼───┼─── ┃ ───┼───┼─── ┃ ───┼───┼───\r
+   │   │    ┃    │   │    ┃    │   │   \r
+───┼───┼─── ┃ ───┼───┼─── ┃ ───┼───┼───\r
+   │   │    ┃    │   │    ┃    │   │   \r
+━━━━━━━━━━━━╋━━━━━━━━━━━━━╋━━━━━━━━━━━━\r
+   │   │    ┃    │   │    ┃    │   │   \r
+───┼───┼─── ┃ ───┼───┼─── ┃ ───┼───┼───\r
+   │   │    ┃    │   │    ┃    │   │   \r
+───┼───┼─── ┃ ───┼───┼─── ┃ ───┼───┼───\r
+   │   │    ┃    │   │    ┃    │   │   \r
+━━━━━━━━━━━━╋━━━━━━━━━━━━━╋━━━━━━━━━━━━\r
+   │   │    ┃    │   │    ┃    │   │   \r
+───┼───┼─── ┃ ───┼───┼─── ┃ ───┼───┼───\r
+   │   │    ┃    │   │    ┃    │   │   \r
+───┼───┼─── ┃ ───┼───┼─── ┃ ───┼───┼───\r
+   │   │    ┃    │   │    ┃    │   │   \r
+";
+
 fn main() {
-    println!("Hello, world!");
+   // Enter raw mode.
+   let mut stdout = stdout().into_raw_mode().unwrap();
+
+   write!(
+      stdout,
+      "{clear}{move}{board}",
+      clear = clear::All,
+      move = cursor::Goto(1, 1),
+      board = BOARD_DISPLAY
+   )
+   .unwrap();
+
+   stdout.flush().unwrap();
 }
