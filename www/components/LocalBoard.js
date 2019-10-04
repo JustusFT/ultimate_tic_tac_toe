344f68f647f0a405b47fc726d8ed89b5e6b4946a
Justus Tumacder <JustusFT@gmail.com>
2019-10-04 15:44:23 +0800

    Add overlay for claimed local boards

--- www/components/LocalBoard.js
+++ www/components/LocalBoard.js
@@ -5,6 +5,8 @@ import Cell from './Cell';
 import { GameContext } from './Game';
 
 const Grid = styled.div`
+  height: 100%;
+  box-sizing: border-box;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   ${props => generateBorders(props.index, '2px solid #666')}
@@ -12,21 +14,54 @@ const Grid = styled.div`
   background-color: ${props => (props.active ? 'lightblue' : 'none')};
 `;
 
+const ClaimerOverlay = styled.div`
+  height: 100%;
+  font-size: 100px;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LayerContainer = styled.div`
+  height: 100%;
+  position: relative;
+`;
+
+const Layer = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`;
+
 export default function LocalBoard({ data, boardIndex }) {
   const { game } = useContext(GameContext);
   const active =
     game.current_board === null || game.current_board === boardIndex;
 
+  const legal = !data.claimer && active;
+
   return (
-    <Grid index={boardIndex} active={active}>
-      {data.board.map((cell, cellIndex) => (
-        <Cell
-          boardIndex={boardIndex}
-          cellIndex={cellIndex}
-          piece={cell}
-          active={active}
-        />
-      ))}
-    </Grid>
+    <LayerContainer>
+      <Layer>
+        <Grid index={boardIndex} active={legal}>
+          {data.board.map((cell, cellIndex) => (
+            <Cell
+              boardIndex={boardIndex}
+              cellIndex={cellIndex}
+              piece={cell}
+              active={legal}
+            />
+          ))}
+        </Grid>
+      </Layer>
+      {data.claimer && (
+        <Layer>
+          <ClaimerOverlay>{data.claimer}</ClaimerOverlay>
+        </Layer>
+      )}
+    </LayerContainer>
   );
 }
