95123eb073459a88178b84776d99f239041a3a0a
Justus Tumacder <JustusFT@gmail.com>
2019-10-07 14:17:46 +0800

    Add big piece display to terminal app

--- terminal_game/src/main.rs
+++ terminal_game/src/main.rs
@@ -1,6 +1,7 @@
 use base_game;
 use game_ai;
 use rustyline::Editor;
+use std::convert::TryFrom;
 use std::io::{stdout, Write};
 use termion::clear;
 use termion::cursor;
@@ -26,12 +27,30 @@ const BOARD_DISPLAY: &'static str = "   \
    │   │    ┃    │   │    ┃    │   │   \r
 ";
 
+type BigPieceStringArray = [&'static str; 5];
+
+const BIG_X: BigPieceStringArray = [
+    " █▄│   │▄█ ",
+    "──▀█▄─▄█▀──",
+    "   │███│   ",
+    "──▄█▀─▀█▄──",
+    " █▀│   │▀█ ",
+];
+
+const BIG_O: BigPieceStringArray = [
+    "  ▄█▀▀▀█▄  ",
+    "─█▀┼───┼▀█─",
+    " █ │   │ █ ",
+    "─█▄┼───┼▄█─",
+    "  ▀█▄▄▄█▀  ",
+];
+
 // these mark the coordinates where the top-left cell of a local board is located from the BOARD_DISPLAY
-const X_CORNERS: [u16; 3] = [2, 16, 30];
-const Y_CORNERS: [u16; 3] = [1, 7, 13];
+const X_CORNERS: [usize; 3] = [2, 16, 30];
+const Y_CORNERS: [usize; 3] = [1, 7, 13];
 // these mark the distance to the other cells of the local board, starting from the top left cell of the local board
-const X_OFFSETS: [u16; 3] = [0, 4, 8];
-const Y_OFFSETS: [u16; 3] = [0, 2, 4];
+const X_OFFSETS: [usize; 3] = [0, 4, 8];
+const Y_OFFSETS: [usize; 3] = [0, 2, 4];
 
 // converts board number into 2D coords (x, y)
 // 0 is (0, 0), 8 is (2, 2)
@@ -72,12 +91,34 @@ fn draw_piece(
       stdout,
       "{move}{piece}",
       move = cursor::Goto(
-        X_CORNERS[corner_x] + X_OFFSETS[offset_x],
-        Y_CORNERS[corner_y] + Y_OFFSETS[offset_y]
+        u16::try_from(X_CORNERS[corner_x] + X_OFFSETS[offset_x]).ok().unwrap(),
+        u16::try_from(Y_CORNERS[corner_y] + Y_OFFSETS[offset_y]).ok().unwrap()
       ),
       piece = piece_to_char(piece)
     )
     .unwrap();
+}
+
+// for claimed boards, draw a big piece on top the board display
+fn draw_big_piece(
+    stdout: &mut termion::raw::RawTerminal<std::io::Stdout>,
+    local_board: usize,
+    overlay: BigPieceStringArray,
+) {
+    let (corner_x, corner_y) = board_coordinates(local_board);
+
+    for i in 0usize..5 {
+        write!(
+            stdout,
+            "{move}{line_text}",
+            move = cursor::Goto(
+                u16::try_from(X_CORNERS[corner_x] - 1).ok().unwrap(),
+                u16::try_from(Y_CORNERS[corner_y] + i).ok().unwrap()
+            ),
+            line_text = overlay[i]
+        )
+        .unwrap();
+    }
 }
 
 // re-draw the whole board
@@ -92,8 +133,16 @@ fn draw_board(game: &base_game::Game, stdout: &mut termion::raw::RawTerminal<std
     .unwrap();
 
     for i in 0..=8 {
-        for j in 0..=8 {
-            draw_piece(stdout, game.local_boards[i].board[j], i, j)
+        match game.local_boards[i].claimer {
+            // for claimed boards, draw a big piece over the board
+            // for non-claimed boards, draw what pieces are currently on it
+            Some(base_game::Piece::X) => draw_big_piece(stdout, i, BIG_X),
+            Some(base_game::Piece::O) => draw_big_piece(stdout, i, BIG_O),
+            _ => {
+                for j in 0..=8 {
+                    draw_piece(stdout, game.local_boards[i].board[j], i, j)
+                }
+            }
         }
     }
 
@@ -152,7 +201,10 @@ fn main() {
 
     loop {
         draw_board(&game, &mut stdout);
+        println!("\rYour turn.");
         request_user_move(&mut game);
+        draw_board(&game, &mut stdout);
+        println!("\rCPU is thinking...");
         let (best_move_a, best_move, _) = game_ai::negamax(&mut game, 5, -3000, 3000, -1);
         game.make_move(best_move_a.unwrap(), best_move.unwrap());
     }
