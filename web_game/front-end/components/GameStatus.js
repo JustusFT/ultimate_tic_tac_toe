a27d1e9df3b447561cd90dc446a6eb216d199b76
Justus Tumacder <JustusFT@gmail.com>
2019-10-15 12:17:27 +0800

    Add win messages to GameStatus

--- web_game/front-end/components/GameStatus.js
+++ web_game/front-end/components/GameStatus.js
@@ -1,16 +1,47 @@
 import React from 'react';
 
+function getCpuStatusMessage({ gameMode, game }) {
+  switch (game.winner) {
+    case 'X':
+    case 'O': {
+      return `${
+        gameMode.playerPiece === game.winner ? 'Player' : 'CPU'
+      } won the game.`;
+    }
+    case 'BLANK': {
+      return 'The game is a draw.';
+    }
+    case null: {
+      return game.turn === gameMode.playerPiece
+        ? 'Your turn'
+        : 'CPU is thinking...';
+    }
+  }
+}
+
+function getLocalTwoPlayerStatusMessage({ gameMode, game }) {
+  switch (game.winner) {
+    case 'X':
+    case 'O': {
+      return `${game.winner} won the game.`;
+    }
+    case 'BLANK': {
+      return 'The game is a draw.';
+    }
+    case null: {
+      return `${game.turn} to move.`;
+    }
+  }
+}
+
 export default function GameStatus({ gameMode, game }) {
   return (
     <div>
-      {gameMode.type === 'vsCpu' && (
-        <p>
-          {game.turn === gameMode.playerPiece
-            ? 'Your turn'
-            : 'CPU is thinking...'}
-        </p>
-      )}
-      {gameMode.type === 'localTwoPlayer' && <p>{game.turn} to move</p>}
+      <p>
+        {gameMode.type === 'vsCpu' && getCpuStatusMessage({ gameMode, game })}
+        {gameMode.type === 'localTwoPlayer' &&
+          getLocalTwoPlayerStatusMessage({ gameMode, game })}
+      </p>
     </div>
   );
 }
